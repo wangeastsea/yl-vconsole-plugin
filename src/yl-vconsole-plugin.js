711d5711ce4e45fa81cef3996eb0873fe0319805
wangeastsea <wangeastsea@icloud.com>
2021-03-24 09:50:44 +0800

    add op

--- src/yl-vconsole-plugin.js
+++ src/yl-vconsole-plugin.js
@@ -5,7 +5,9 @@
  */
 
 // import './style.less';
-
+const getStorageBykey =  function (key, type) {
+    return window.localStorage.getItem(`${key}`) && window.localStorage.getItem('userToken').replace(`${type}@`, '') 
+}
 class VConsoleYlPlugin {
     constructor(vConsole) {
         this.vConsole = vConsole;
@@ -14,7 +16,7 @@ class VConsoleYlPlugin {
 
     init() {
         const switchEnv = new window.VConsole.VConsolePlugin('switchEnv', '切换连接环境');
-
+        let userId = getStorageBykey('userId' , 'number')
         switchEnv.on('init', function() {
             console.log('My plugin init')
         })
@@ -25,6 +27,10 @@ class VConsoleYlPlugin {
                             <div style="padding:15px;">
                                 <span style="font-size: 20px;color: orange">当前链接：</span>
                                 <div id="url" style="padding:10px;font-size: 16px;background-color:#f7f8fa;">${window.location.href}</div>
+                            </div>
+                            <div style="padding:15px;">
+                                <span style="font-size: 20px;color: orange">用户ID：</span>
+                                <div id="userId" style="padding:10px;font-size: 16px;background-color:#f7f8fa;">${userId}</div>
                             </div>
                         </div>`
             callback(html)
@@ -76,17 +82,43 @@ class VConsoleYlPlugin {
             })
             callback(btnList)
         })
-        // 底部的按钮
+        // 底部的按钮 - 刷新
         switchEnv.on('addTool', function(callback) {
-            var button = {
+            let Reload = {
                 name: 'Reload',
                 onClick: function(e) {
                     location.reload()
                     console.log('e', e)
                 }
             }
-            callback([button])
+            let delToken = {
+                name: 'delToken',
+                onClick: function(e) {
+                    window.localStorage.removeItem('userToken')
+                    console.log('e', e)
+                }
+            }
+            let cpToken = {
+                name: 'cpToken',
+                onClick: function(e) {
+                    let textarea = document.createElement('textarea')
+                    let userToken = getStorageBykey('userToken' , 'string')
+                    textarea.value = userToken
+                    document.body.appendChild(textarea)
+                    textarea.select()
+                    let isCopy = document.execCommand('copy')//isCopy可以表示是否调用execCommand()成功
+                    if (isCopy) {
+                        window.alert('copy success')
+                        console.log('copy success')
+                    }
+                    document.body.removeChild(textarea)
+                    console.log('e', e)
+                }
+            }
+            callback([Reload, cpToken, delToken])
         })
+        // 底部的按钮 - 删除用户token
+        // 底部的按钮 - 复制用户token
         this.vConsole.addPlugin(switchEnv);
         return switchEnv;
     }
